870d0b0395f2dc1b4f4ffd3c1a13fb347dcbdc06
HeloisadoVale <29806950+HeloisadoVale@users.noreply.github.com>
2019-11-27 21:24:22 -0300

    Add files via upload

--- script.r
+++ script.r
@@ -8,28 +8,29 @@ libraryRequireInstall("dplyr")
 libraryRequireInstall("lubridate")
 libraryRequireInstall("anytime")
 libraryRequireInstall("dygraphs")
+libraryRequireInstall("magrittr")
+
 ####################################################
 
 ################### Actual code ####################
 
-#conversão de tipos
-dataset$usu_cmporc <- anytime(dataset$usu_cmporc) 
-dataset$usu_cmpini <- anytime(dataset$usu_cmpini) 
-dataset$usu_cmpfim <- anytime(dataset$usu_cmpfim) 
+#conversÃ£o de tipos
+Values$usu_cmporc <- anytime(Values$usu_cmporc) 
+
 
 ##agrupamento mensal
-orc_agrup <- dataset %>% group_by(month=floor_date(usu_cmporc, "month")) %>% 
+orc_agrup <- Values %>% group_by(month=floor_date(usu_cmporc, "month")) %>% 
   summarise(amount=sum(usu_valrea))
 
-##transformação dos dados
+##transformaÃ§Ã£o dos dados
 
-#1) separação dos conjuntos de teste e treino
+#1) separaÃ§Ã£o dos conjuntos de teste e treino
 
 #dados de treinamento:
 dados_treino <- orc_agrup %>% slice(1:(n()-2))
 
 
-#2) transformação logarítmica
+#2) transformaÃ§Ã£o logarÃ­tmica
 
 #montar time series
 dados_treino_ts <- ts(
@@ -43,7 +44,7 @@ log_dados_treino <- log(dados_treino_ts)
 
 ## Modelo 
 
-# função para selecionar melhores parametros para o modelo
+# funÃ§Ã£o para selecionar melhores parametros para o modelo
 best_sarima_model <- function(train_data,p,q,P,Q,d=1,D=1,s=12){
   
   current_best_model = arimax(
@@ -91,11 +92,11 @@ best_sarima_model <- function(train_data,p,q,P,Q,d=1,D=1,s=12){
   return (current_best_model)
 }
 
-#aplicação do modelo selecionado
+#aplicaÃ§Ã£o do modelo selecionado
 best_model = best_sarima_model(train_data = log_dados_treino,p=range(0,2),q=range(0,2),P=range(0,2),Q=range(0,2))
 
 
-### visualização
+### visualizaÃ§Ã£o
 interval_value_formatter <- "function(num, opts, seriesName, g, row, col) {
   value = g.getValue(row, col);
   if(value[0] != value[2]) {
@@ -115,31 +116,33 @@ orc_agrup_ts <- ts(
   frequency = 12)
 log_orc_agrup_ts <- log(orc_agrup_ts)
 
+
 ## based on: https://towardsdatascience.com/how-to-create-better-interactive-forecast-plots-using-r-and-dygraph-29bdd7146066
-p = log_orc_agrup_ts %>%
-  forecast(model = best_model, h = 6)%>%
-  {cbind(
-    actuals=round(exp(.$x)/1000000,3),
-    forecast_mean=round(exp(.$mean)/1000000,3),
-    lower_95=round(exp(.$lower[,"95%"])/1000000,3), 
-    upper_95=round(exp(.$upper[,"95%"])/1000000,3),
-    lower_80=round(exp(.$lower[,"80%"])/1000000,3), 
-    upper_80=round(exp(.$upper[,"80%"])/1000000,3))} %>%
-  dygraph(ylab = "Valor Realizado (R$) - em Milhões") %>%
-  dyAxis("y", valueFormatter = interval_value_formatter) %>%
-  dySeries("actuals", color = "#002C47", label = "Realizado",drawPoints = TRUE, pointSize = 3,strokeWidth = 2) %>%
-  dySeries("forecast_mean", color = "#EC6826", label = "Forecast",drawPoints = TRUE, pointSize = 3, strokeWidth = 2) %>%
-  dySeries(c("lower_80", "forecast_mean", "upper_80"),
-           label = "Int. Confiança - 80%", color = "#EC6826") %>%
-  dySeries(c("lower_95", "forecast_mean", "upper_95"),
-           label = "Int. Confiança - 95%", color = "#EC6826") %>%
-  dyLegend(labelsSeparateLines=TRUE) %>%
-  dyRangeSelector() %>%
-  dyOptions(digitsAfterDecimal = 1) %>%
-  dyCSS(textConnection(".dygraph-legend {background-color: rgba(255, 255, 255, 0.5) !important; }"))
+p <- log_orc_agrup_ts %>%
+    forecast(model = best_model, h = Values$projecao_meses)%>%
+    {cbind(
+      actuals=round(exp(.$x)/1000000,3),
+      forecast_mean=round(exp(.$mean)/1000000,3),
+      lower_95=round(exp(.$lower[,"95%"])/1000000,3), 
+      upper_95=round(exp(.$upper[,"95%"])/1000000,3),
+      lower_80=round(exp(.$lower[,"80%"])/1000000,3), 
+      upper_80=round(exp(.$upper[,"80%"])/1000000,3))} %>%
+      dygraph(ylab = "Valor Realizado (R$) - em Milhoes") %>%
+      dyAxis("y", valueFormatter = interval_value_formatter) %>%
+      dySeries("actuals", color = "#002C47", label = "Realizado",drawPoints = TRUE, pointSize = 3,strokeWidth = 2) %>%
+      dySeries("forecast_mean", color = "#EC6826", label = "Forecast",drawPoints = TRUE, pointSize = 3, strokeWidth = 2) %>%
+      dySeries(c("lower_80", "forecast_mean", "upper_80"),
+           label = "Intervalo Conf. - 80%", color = "#EC6826") %>%
+      dySeries(c("lower_95", "forecast_mean", "upper_95"),
+           label = "Intervalo Conf. - 95%", color = "#EC6826") %>%
+      dyLegend(labelsSeparateLines=TRUE) %>%
+      dyRangeSelector() %>%
+      dyOptions(digitsAfterDecimal = 1) %>%
+      dyCSS(textConnection(".dygraph-legend {background-color: rgba(255, 255, 255, 0.5) !important; }"))
 
 ####################################################
 
 ############# Create and save widget ###############
-internalSaveWidget(p, 'out.html');
+
+internalSaveWidget(p,'out.html')
 ####################################################
